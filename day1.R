9066340b6976a94ca49e04d989498547dbc01006
jzeph <jzeph@users.noreply.github.com>
2018-12-01 09:40:59 -0600

    Create day1.R

---
+++ day1.R
@@ -0,0 +1,6 @@
+# Read in data
+# Needed to copy/paste to github txt file to bypass oauth
+df <- read.table("https://raw.githubusercontent.com/jzeph/advent-of-code-2018/master/day1input.txt")
+
+# Get sum
+sum(df)
